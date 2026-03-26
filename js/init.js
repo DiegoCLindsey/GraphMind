df333e3b63833ae114a5dd4b564f1d6a62f3f34d
d.camacho <d.camacho@sportradar.com>
2026-03-26 14:39:13 +0100

    refactor(GMND-015): change init to use applySnapshot helper

--- js/init.js
+++ js/init.js
@@ -8,12 +8,7 @@ function init() {
     const raw = localStorage.getItem(LS_KEY);
     if (raw) {
       const data = JSON.parse(raw);
-      if (data && data.nodes) {
-        S.nodes = data.nodes;
-        tagColorMap = data.tagColorMap || {};
-        tci = data.tci || Object.keys(tagColorMap).length;
-        S.currentId = S.nodes.length ? S.nodes[0].id : null;
-      }
+      if (data && data.nodes) applySnapshot(data);
     }
   } catch(e) { console.warn('GraphMind: error al cargar localStorage', e); }
   renderList(); renderEditor(); updateCount();
